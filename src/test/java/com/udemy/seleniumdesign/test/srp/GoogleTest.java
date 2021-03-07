fe9e0710e09e4b11bbf65f6a1df4c2b0bcd87e70
Thiago Castilho <tcosta1996@hotmail.com>
2021-03-07 03:57:09 -0300

    season 2: google search test - part 2

--- src/test/java/com/udemy/seleniumdesign/test/srp/GoogleTest.java
+++ src/test/java/com/udemy/seleniumdesign/test/srp/GoogleTest.java
@@ -20,8 +20,29 @@ public class GoogleTest extends BaseTest {
 
     @Test
     public void googleWorkflow() {
+
+        String keyword = "selenium webdriver";
+        int index = 3;
+
         googleMainPage.goTo();
         Assert.assertTrue(googleMainPage.getSearchWidget().isDisplayed());
+
+        googleMainPage.getSearchWidget().enter(keyword);
+        Assert.assertTrue(googleMainPage.getSearchSuggestion().isDisplayed());
+
+        googleMainPage.getSearchSuggestion().clickSuggestionByIndex(index);
+        Assert.assertTrue(googleResultPage.getNavigationBar().isDisplayed());
+
+        googleResultPage.getSearchWidget().enter(keyword);
+        Assert.assertTrue(googleResultPage.getSearchSuggestion().isDisplayed());
+
+        googleResultPage.getSearchSuggestion().clickSuggestionByIndex(index);
+
+        googleResultPage.getNavigationBar().goToNews();
+
+        System.out.println(
+                googleResultPage.getResultStat().getStat()
+        );
     }
 
 
