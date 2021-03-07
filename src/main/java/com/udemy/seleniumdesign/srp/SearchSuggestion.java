bb804eb01c258e098c4ea2348cbbe4e7fcd61a42
Thiago Castilho <tcosta1996@hotmail.com>
2021-03-07 03:14:31 -0300

    season 2: refactoring

--- src/main/java/com/udemy/seleniumdesign/srp/SearchSuggestion.java
+++ src/main/java/com/udemy/seleniumdesign/srp/SearchSuggestion.java
@@ -3,22 +3,16 @@ package com.udemy.seleniumdesign.srp;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
 public class SearchSuggestion extends AbstractComponent {
 
-    private WebDriverWait wait;
-
     @FindBy(css = "li.sbct")
     private List<WebElement> suggestions;
 
     public SearchSuggestion(final WebDriver driver) {
-        this.wait = new WebDriverWait(driver, 30);
-        PageFactory.initElements(driver, this);
+        super(driver);
     }
 
     public void clickSuggestionByIndex(int index) {
--- src/main/java/com/udemy/seleniumdesign/srp/SearchWidget.java
+++ src/main/java/com/udemy/seleniumdesign/srp/SearchWidget.java
@@ -3,20 +3,14 @@ package com.udemy.seleniumdesign.srp;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SearchWidget extends AbstractComponent {
-
-    private WebDriverWait wait;
 
     @FindBy(name = "q")
     private WebElement searchBox;
 
     public SearchWidget(final WebDriver driver) {
-        this.wait = new WebDriverWait(driver, 30);
-        PageFactory.initElements(driver, this);
+        super(driver);
     }
 
     public void enter(String keyword) {
@@ -26,8 +20,7 @@ public class SearchWidget extends AbstractComponent {
 
     @Override
     public boolean isDisplayed() {
-        this.wait.until(ExpectedConditions.visibilityOf(this.searchBox));
-        return this.searchBox.isDisplayed();
+        return this.wait.until((driver) -> this.searchBox.isDisplayed());
     }
 
 }
