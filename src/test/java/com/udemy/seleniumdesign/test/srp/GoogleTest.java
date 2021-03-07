70b9901bca2d93ab6fd9c96123ff5e7150f3841c
Thiago Castilho <tcosta1996@hotmail.com>
2021-03-07 03:50:55 -0300

    season 2: google search test - part 1

---
+++ src/test/java/com/udemy/seleniumdesign/test/srp/GoogleTest.java
@@ -0,0 +1,28 @@
+package com.udemy.seleniumdesign.test.srp;
+
+import com.udemy.seleniumdesign.srp.GoogleMainPage;
+import com.udemy.seleniumdesign.srp.GoogleResultPage;
+import com.udemy.seleniumdesign.test.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class GoogleTest extends BaseTest {
+
+    private GoogleMainPage googleMainPage;
+    private GoogleResultPage googleResultPage;
+
+    @BeforeTest
+    public void setupPages() {
+        this.googleMainPage = new GoogleMainPage(driver);
+        this.googleResultPage = new GoogleResultPage(driver);
+    }
+
+    @Test
+    public void googleWorkflow() {
+        googleMainPage.goTo();
+        Assert.assertTrue(googleMainPage.getSearchWidget().isDisplayed());
+    }
+
+
+}
